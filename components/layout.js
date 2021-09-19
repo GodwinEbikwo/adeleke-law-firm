2785ac32e919f9b27a2575fc43998db2ed30d91d
“GodwinEbikwo” <godwin.a.ebikwo@gmail.com>
2021-09-19 08:21:15 +0100

    layout head tags added

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,16 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>The Adeleke Brothers - Law Firm</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:locale" content="en_GB" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Adeleke Family Law is Nigerias largest specialist family law firm. Our divorce solicitors enjoy an outstanding reputation for expertise, compassion &amp; success."
+        />
+        <title>
+          Divorce Solicitors &amp; Family Lawyers Across Nigeria - Adeleke Family Law
+        </title>
       </Head>
       {children}
     </>
